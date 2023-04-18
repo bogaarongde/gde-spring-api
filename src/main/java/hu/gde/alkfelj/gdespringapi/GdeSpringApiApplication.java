d2068c2d318159128d936157d06e6919571e5c16
Aron Boga <aron.boga@epsglobal.eu>
2023-04-18 15:41:39 +0200

    Create project

---
+++ src/main/java/hu/gde/alkfelj/gdespringapi/GdeSpringApiApplication.java
@@ -0,0 +1,13 @@
+package hu.gde.alkfelj.gdespringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdeSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdeSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/gde/alkfelj/gdespringapi/GdeSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gde.alkfelj.gdespringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdeSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
